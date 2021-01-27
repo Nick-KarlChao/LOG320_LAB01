d6218a472b946ee0f659db125e60021984e32735
imeeveemi <ai-vi.nguyen.1@ens.etsmtl.ca>
2021-01-27 11:43:48 -0500

    Class setup

---
+++ src/Algorithm/Huffman.java
@@ -0,0 +1,27 @@
+package Algorithm;
+
+import Algorithm.AbstractAlgorithm;
+
+public class Huffman extends AbstractAlgorithm {
+    @Override
+    public void compress() {
+        //TODO
+    }
+
+    @Override
+    public void decompress() {
+        //TODO
+    }
+
+    private void createFrequencyTable(){
+        //TODO
+    };
+
+    private void createHuffmanTable(){
+        //TODO
+    };
+
+    private void encode(){
+        //TODO
+    };
+}
